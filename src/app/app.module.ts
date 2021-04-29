ad042e64048971222ea093b81c51ef5d6a295c06
camilla.silva <camilla.silva@take.net>
2021-04-29 17:46:31 -0300

    criação inicial do menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './Components/clientes/clientes.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule}  from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { CadastroComponent } from './Components/cadastro/cadastro.component';
+import { MenuComponent } from './Components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
+    ClientesComponent,
+    CadastroComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +27,7 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     MatInputModule,
     MatButtonModule,
+    MatMenuModule,
     BrowserAnimationsModule
   ],
   providers: [],
